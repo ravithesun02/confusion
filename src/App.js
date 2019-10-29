aae50873bf607fab207c7fa05b4584f16b63a9a1
Ravi Kumar <ravithesun02@gmail.com>
2019-10-29 21:26:07 +0530

    menu components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React ,{ Component } from 'react';
 import {Navbar,NavbarBrand} from 'reactstrap';
 import logo from './logo.svg';
+import Menu from './components/MenuComponents';
 import './App.css';
+import { DISHES } from './shared/DISHES';
 
-function App() {
+class App extends Component {
+
+  constructor(props)
+  {
+    super(props);
+
+    this.state={
+      dishes:DISHES
+    };
+  }
+render(){
   return (
-    <div className="App">
+    <div>
       <Navbar dark color="primary">
         <div className="container">
           <NavbarBrand href="/">Confusion</NavbarBrand>
         </div>
       </Navbar>
-      
+      <Menu dishes={this.state.dishes}/>
     </div>
   );
+}
 }
 
 export default App;
